b2a73e3f6f2974c1bd69c76948817b4e40c3759e
jaisondbv <57779228+jaisondbv@users.noreply.github.com>
2025-05-14 10:20:56 -0500

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pymongo import MongoClient
+import pandas as pd
+import os
+
+MONGO_URI = "mongodb+srv://ketihe1596:V1in9OG26SRXZqCZ@cluster0.a4qkpgi.mongodb.net/"
+client = MongoClient(MONGO_URI)
+db = client['kaggle_db']
+collection = db['datos_kaggle']
+
+# Cargar datos
+data = list(collection.find({}, {"_id": 0}))  # omitir el _id de mongo
+df = pd.DataFrame(data)
+
+st.title("Dashboard desde MongoDB Atlas")
+st.write(df)
